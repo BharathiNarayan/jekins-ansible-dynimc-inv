ee2b2db3750a7195bcad955ee8dd18862a8e40bb
BharathiNarayan <gsbharathinarayan@gmail.com>
2024-11-07 11:49:49 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,11 +1,11 @@
 provider "aws"{
-region = "eu-west-2"
+region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSINSTANCE"{
-ami = "ami-0acc77abdfc7ed5a6"
+resource "aws_instannce" "AWSINSTANCE"{
+ami = "ami-08bf489a05e916bbd"
 instance_type = "t2.micro"
-key_name = "devopsaug2024"
+key_name = "AnsibleMachine"
 security_groups = ["launch-wizard-2"]
 tags={
 Name = "terraformserver"
